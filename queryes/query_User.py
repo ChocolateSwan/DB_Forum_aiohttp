6ec298a8b2b18cb172c2da444c2e7015b325f57b
ChocolateSwan <olyasur1996@gmale.com>
2017-11-05 16:32:58 +0300

    refactor

--- queryes/query_User.py
+++ queryes/query_User.py
@@ -11,3 +11,24 @@ create_user = '''with try_insert as (INSERT INTO "User" (about, email, fullname,
                 where nickname = $4 or email = $2'''
 
 get_user = '''SELECT about, email, fullname, nickname FROM "User" where nickname = $1'''
+
+up_u = '''with conflict_email as (
+                select id,about,email,fullname,nickname, false from "User" where email = {}
+                ), 
+                
+                '''
+
+up_u_2 = '''
+union all
+select * from conflict_email
+                
+                '''
+
+update_user = '''
+try_update as (
+                UPDATE "User" set {} where nickname = {} and {} not in (select email from conflict_email)
+                returning id, about, email,fullname, nickname, true
+                )
+                
+                select * from try_update'''
+
--- views/views_User.py
+++ views/views_User.py
@@ -1,5 +1,6 @@
 from aiohttp.web import json_response, View
-from query_User import create_user, get_user
+
+from queryes.query_User import create_user, get_user, update_user, up_u, up_u_2
 
 
 class Test(View):
@@ -49,5 +50,36 @@ class UserProfile (View):
                                                    self.request.match_info['nickname'])
                 status = 404 if result is None else 200
                 return json_response({'message': 'cant find user'} if status == 404 else dict(result),
+                                     status=status)
+
+    async def post(self):
+        pool = self.request.app['pool']
+        async with pool.acquire() as connection:
+            async with connection.transaction():
+                # TODO добавить default в гет чтоб исключение не бросало
+                data = await  self.request.json()
+                req_update_user = ' '
+                for key in data:
+                    req_update_user += key + " = '" + str(data[key]) + "', "
+                req_update_user = req_update_user[:req_update_user.rfind(',')]
+                print (req_update_user)
+                update_u = update_user.format(req_update_user, "'"+self.request.match_info['nickname']+"'", "'"+data.get('email',' ') + "'")
+                update_u = up_u.format( "'"+data.get('email',' ') + "'" if data.get('email','') else ''
+                    # "'"+data.get('email',' ') + "'"
+                                        ) + update_u + up_u_2
+                print (update_u)
+                result = await connection.fetchrow(update_u)
+                print (result)
+                result = dict(result)
+                # {'message': 'cant find user'}
+                status = 200
+                if result['bool'] == True:
+                    status = 200
+                else:
+                    status = 409
+
+
+
+                return json_response(result,
                                      status=status)
 
